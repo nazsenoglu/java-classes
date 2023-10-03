c750a99cafe8093f581d5b3f17fe16694cede44e
nazsenoglu <aysenazsenoglu@gmail.com>
2023-10-03 15:17:49 -0700

    oct 3

---
+++ main/src/DivisibleByThreeSum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DivisibleByThreeSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int sum = 0;
+
+        System.out.println("Enter whole numbers (enter 0 to stop):");
+
+        do {
+            number = scanner.nextInt();
+            if (number % 3 == 0) {
+                sum += number;
+            }
+        } while (number != 0);
+
+        System.out.println("Sum of numbers divisible by 3: " + sum);
+    }
+}
