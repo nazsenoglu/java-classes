446eb83400ab9197c0fd2c82df25f700444918c9
nazsenoglu <aysenazsenoglu@gmail.com>
2023-10-09 14:36:03 -0700

    9.10.2023

---
+++ main/src/bankInterest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class bankInterest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of years: ");
+        int years = scanner.nextInt();
+
+        double initialAmount = 100.0;
+        double interestRate = 0.15;
+
+        double totalAmount = initialAmount * Math.pow(1 + interestRate, years);
+
+        System.out.println("Total money after " + years + " years: $" + totalAmount);
+    }
+}
