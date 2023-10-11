c4ba7c2a7ba8c4d56bb9f7a71b97c38e5917d94d
nazsenoglu <aysenazsenoglu@gmail.com>
2023-10-10 19:50:49 -0700

    10.10.2023

---
+++ main/src/SumCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        int sumResult = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sumResult += i;
+            i++;
+        }
+
+        System.out.println("The sum of all positive integers less than or equal to " + n + " is " + sumResult);
+    }
+}
