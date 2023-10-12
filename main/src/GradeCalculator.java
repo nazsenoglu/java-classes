e3b62e9cf23af0a8ff16efd10e4bb2332b96865c
nazsenoglu <aysenazsenoglu@gmail.com>
2023-10-12 14:09:21 -0700

    10.12.2023

---
+++ main/src/GradeCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the numeric score: ");
+        int score = scanner.nextInt();
+        String grade;
+
+        if (score >= 90 && score <= 100) {
+            grade = "A";
+        } else if (score >= 80 && score <= 89) {
+            grade = "B";
+        } else if (score >= 70 && score <= 79) {
+            grade = "C";
+        } else if (score >= 60 && score <= 69) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("The corresponding letter grade is: " + grade);
+    }
+}
---
+++ main/src/Practice1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+
+        int myNum = input.nextInt();
+        while (myNum < 0) {
+            System.out.println("Please enter a  positive number");
+            myNum = input.nextInt();
+        }
+
+        int result = 1;
+
+        for (int i = 1; i < myNum; i++) {
+            result = result * i;
+        }
+        System.out.println("The factorial of" + myNum + ":" + result);
+    }
+}
