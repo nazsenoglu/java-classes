7aa39f6d4563229a582197b88ca21fc4bffb8020
nazsenoglu <aysenazsenoglu@gmail.com>
2023-11-17 14:00:41 -0800

    11.17.2023

--- main/src/MultiplicationTable.java
+++ main/src/MultiplicationTable.java
@@ -1,17 +1,20 @@
+import java.util.Scanner;
 
 public class MultiplicationTable {
-
     public static void main(String[] args) {
-        int product;
-        for (int i = 1; i <= 10; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            for (int j = i; j <= 10; j++) {
-                product = i * j;
-                System.out.println(i + " x " + j + " = " + product);
+        System.out.print("Please enter a number: ");
+        int enteredNumber = scanner.nextInt();
+        System.out.println("Below is the table from 1 to " + enteredNumber);
+
+        for (int i = 1; i <= enteredNumber; i++) {
+            for (int j = 1; j <= enteredNumber; j++) {
+
+                System.out.printf("%4d", i * j);
             }
-            System.out.println("");
+            System.out.println();
         }
-
+        scanner.close();
     }
-
 }
---
+++ main/src/MultiplicationTable1.java
@@ -0,0 +1,17 @@
+
+public class MultiplicationTable1 {
+
+    public static void main(String[] args) {
+        int product;
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = i; j <= 10; j++) {
+                product = i * j;
+                System.out.println(i + " x " + j + " = " + product);
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
